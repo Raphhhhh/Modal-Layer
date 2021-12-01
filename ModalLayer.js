204a6bab487ab3e0eef050bf5fb1aca5a1d183f6
Raphaël Prat <raphael@tokywoky.com>
2021-12-01 11:29:52 +0100

    init toky

--- ModalLayer.js
+++ ModalLayer.js
@@ -1,12 +1,13 @@
 export class ModalLayer {
   constructor(options = {}) {
-
-    this.modalId = `Modal-${Date.now()}`;
-
     this.contentStyle = '';
     this.contentHTML = '';
-    this.heightModal = options.heightModal || '365px';
-    this.widthModal = options.widthModal || '600px';
+    this.height = options.height || '365px';
+    this.width = options.width || '600px';
+    this.closeButtonContent = options.closeButtonContent || 'X';
+    this.customCss = options.customCss || '';
+
+    this.modalId = `TokyChannelWidgetPopupId-${Date.now()}`;
     
     this.defineStyles();
     this.createModal();
@@ -15,29 +16,32 @@ export class ModalLayer {
   }
 
   defineStyles() {
-    this.modalStyle = `.ModalLayer-modal { height: ${this.heightModal}; width: ${this.widthModal}; background-color: #fff; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); padding: 45px; opacity: 0; pointer-events: none; transition: all 300ms ease-in-out; z-index: 1011; }`;
-    this.modalStyleVisible = '.ModalLayer-modal-visible { opacity: 1; pointer-events: auto; }';
-    this.modalStyleClose = '.ModalLayer-modal-closeBtn { position: absolute; font-size: 1.2rem; right: -10px; top: -10px; cursor: pointer; background:#F30; padding:5px; font-size:bold; border-radius:50%; }';
-    this.blackedout = '.ModalLayer-blackedout { position: absolute; z-index: 1010; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.65); display: none; }';
-    this.blackedoutVisible = `.ModalLayer-blackedout-visible { display: block; height:${document.body.clientHeight}px }`;
+    this.modalStyle = `.TokyChannelWidgetPopup-modal { height: ${this.height}; width: ${this.width}; background-color: #fff; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); padding: 0px; opacity: 0; pointer-events: none; transition: all 300ms ease-in-out; z-index: 1011; }`;
+    this.modalStyleVisible = '.TokyChannelWidgetPopup-modal-visible { opacity: 1; pointer-events: auto; }';
+    this.modalStyleClose = '.TokyChannelWidgetPopup-modal-closeBtn { position: absolute; font-size: 1.2rem; right: -20px; top: -20px; cursor: pointer;  font-size:bold; border-radius:50%; color: white; }';
+    if(this.width === '100%' || this.height === '100%'){
+      this.modalStyleClose += '.TokyChannelWidgetPopup-modal-closeBtn {right: 5px; top: 5px; color: black;}';
+    }
+    this.blackedout = '.TokyChannelWidgetPopup-blackedout { position: absolute; z-index: 1010; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.65); display: none; }';
+    this.blackedoutVisible = `.TokyChannelWidgetPopup-blackedout-visible { display: block; height:${document.body.clientHeight}px }`;
   }
 
   createModal() {
     this.modalHTML = document.createElement('div');
     this.modalHTML.id = this.modalId;
     this.modalHTML.dataset.popupModal = 'one';
-    this.modalHTML.classList.add('ModalLayer-modal');
-    this.modalHTML.classList.add('ModalLayer-shadow');
+    this.modalHTML.classList.add('TokyChannelWidgetPopup-modal');
+    this.modalHTML.classList.add('TokyChannelWidgetPopup-shadow');
     this.bodyBlackedout = document.createElement('div');
-    this.bodyBlackedout.classList.add('ModalLayer-blackedout');
+    this.bodyBlackedout.classList.add('TokyChannelWidgetPopup-blackedout');
   }
 
   injectStyles() {
     this.defineStyles();
-    let style = document.getElementById('EditContentStyles');
+    let style = document.getElementById('TokyChannelWidgetPopupStyles');
     if (!style) {
       style = document.createElement('style');
-      style.setAttribute('id', 'EditContentStyles');
+      style.setAttribute('id', 'TokyChannelWidgetPopupStyles');
       style.setAttribute('type', 'text/css');
       style.innerHTML = this.modalStyle + this.modalStyleVisible + this.blackedout + this.blackedoutVisible;
       document.getElementsByTagName('head')[0].appendChild(style);
@@ -57,9 +61,10 @@ export class ModalLayer {
       <style>
         ${this.modalStyleClose}
         ${this.contentStyle}
+        ${this.customCss}
       </style>
-      <span class="ModalLayer-modal-closeBtn">X</span>
-      <div id="ModalLayer-modal-content">${this.contentHTML}</div>
+      <span class="TokyChannelWidgetPopup-modal-closeBtn">${this.closeButtonContent}</span>
+      <div id="TokyChannelWidgetPopup-modal-content">${this.contentHTML}</div>
     `;
   }
 
@@ -68,17 +73,17 @@ export class ModalLayer {
   }
 
   hideModal() {
-    this.modalHTML.classList.remove('ModalLayer-modal-visible');
-    this.bodyBlackedout.classList.remove('ModalLayer-blackedout-visible');
-    document.querySelector('.ModalLayer-blackedout').removeEventListener('click', this.hideModal);
-    this.modalHTML.shadowRoot.querySelector('.ModalLayer-modal-closeBtn').removeEventListener('click', this.hideModal);
+    this.modalHTML.classList.remove('TokyChannelWidgetPopup-modal-visible');
+    this.bodyBlackedout.classList.remove('TokyChannelWidgetPopup-blackedout-visible');
+    document.querySelector('.TokyChannelWidgetPopup-blackedout').removeEventListener('click', this.hideModal);
+    this.modalHTML.shadowRoot.querySelector('.TokyChannelWidgetPopup-modal-closeBtn').removeEventListener('click', this.hideModal);
   }
 
   openModal() {
-    this.modalHTML.classList.add('ModalLayer-modal-visible');
-    this.bodyBlackedout.classList.add('ModalLayer-blackedout-visible');
+    this.modalHTML.classList.add('TokyChannelWidgetPopup-modal-visible');
+    this.bodyBlackedout.classList.add('TokyChannelWidgetPopup-blackedout-visible');
     this.injectModalContent();
-    this.modalHTML.shadowRoot.querySelector('.ModalLayer-modal-closeBtn').addEventListener('click', this.hideModal.bind(this));
-    document.querySelector('.ModalLayer-blackedout').addEventListener('click', this.hideModal.bind(this));
+    this.modalHTML.shadowRoot.querySelector('.TokyChannelWidgetPopup-modal-closeBtn').addEventListener('click', this.hideModal.bind(this));
+    document.querySelector('.TokyChannelWidgetPopup-blackedout').addEventListener('click', this.hideModal.bind(this));
   }
 }
